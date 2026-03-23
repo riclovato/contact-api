f5d6f7739d84db65dd2d591e32e8704f777ec0af
riclovato <riic.lovato@gmail.com>
2026-03-23 16:45:17 -0300

    first commit

---
+++ src/main/java/com/ricklovato/contact_api/ContactApiApplication.java
@@ -0,0 +1,13 @@
+package com.ricklovato.contact_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactApiApplication.class, args);
+	}
+
+}
