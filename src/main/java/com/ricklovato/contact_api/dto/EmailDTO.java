e1a5a84fd605321e639db4cd3b60f9ff583bd43b
riclovato <riic.lovato@gmail.com>
2026-03-23 17:56:14 -0300

    feat: add EmailService and rename EmailDTO

--- src/main/java/com/ricklovato/contact_api/dto/EmailDTO.java
+++ src/main/java/com/ricklovato/contact_api/dto/EmailDTO.java
@@ -2,7 +2,7 @@ package com.ricklovato.contact_api.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record  Message(
+public record  EmailDTO(
     @Email(message = "Invalid email format")
     @NotBlank(message = "Email is required") String email, 
     @NotBlank(message = "Message is required") String message) {}
---
+++ src/main/java/com/ricklovato/contact_api/service/EmailService.java
@@ -0,0 +1,30 @@
+package com.ricklovato.contact_api.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import com.ricklovato.contact_api.dto.EmailDTO;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+    @Value("${mail.to}")
+    private String mailTo;
+    @Value("${mail.subject}")
+    private String mailSubject;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendEmail(EmailDTO emailDTO) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(mailTo);
+        message.setSubject(mailSubject);
+        message.setText("de: " + emailDTO.email() + "\n\n" + emailDTO.message());
+        mailSender.send(message);
+    }
+}
