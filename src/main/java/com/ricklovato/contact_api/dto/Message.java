8a5f647d5ba1bf72fe07b02db6923a7e6e6459e5
riclovato <riic.lovato@gmail.com>
2026-03-23 17:03:08 -0300

    feat: add basic project structure and Message DTO

---
+++ src/main/java/com/ricklovato/contact_api/dto/Message.java
@@ -0,0 +1,8 @@
+package com.ricklovato.contact_api.dto;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record  Message(
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required") String email, 
+    @NotBlank(message = "Message is required") String message) {}
