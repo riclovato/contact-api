2943fc0b265e0b2e2d38d17967a695d6664aab2f
riclovato <riic.lovato@gmail.com>
2026-03-24 16:17:06 -0300

    refactor: replace Spring Mail with RestTemplate in email service

--- src/main/java/com/ricklovato/contact_api/service/EmailService.java
+++ src/main/java/com/ricklovato/contact_api/service/EmailService.java
@@ -1,31 +1,47 @@
 package com.ricklovato.contact_api.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.MediaType;
+import java.util.List;
 
 import com.ricklovato.contact_api.dto.EmailDTO;
 
 @Service
 public class EmailService {
 
-    private final JavaMailSender mailSender;
+    private final RestTemplate restTemplate;
     @Value("${mail.to}")
     private String mailTo;
     @Value("${mail.subject}")
     private String mailSubject;
+    @Value("${brevo.api.key}")
+    private String apiKey;
 
-    public EmailService(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
+    public EmailService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
     public void sendEmail(EmailDTO emailDTO) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(mailTo);
-        message.setTo(mailTo);
-        message.setSubject(mailSubject);
-        message.setText("de: " + emailDTO.email() + "\n\n" + emailDTO.message());
-        mailSender.send(message);
+
+       
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("api-key", apiKey); 
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("sender", Map.of("email", mailTo));
+        body.put("to", List.of(Map.of("email", mailTo))); 
+        body.put("subject", mailSubject);
+        body.put("textContent", "De: " + emailDTO.email() + "\n\n" + emailDTO.message());
+
+        HttpEntity<Map<String, Object>> request = new HttpEntity<>(body, headers);
+        restTemplate.postForEntity("https://api.brevo.com/v3/smtp/email", request, String.class);
     }
 }
